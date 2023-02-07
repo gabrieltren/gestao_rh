eecc098283340c4d9263cb42b5d733605291d9c5
Gabriel Trentini <gabriel.silveira1207@gmail.com>
2023-02-07 10:32:36 -0300

    Criando a app de Funcionario

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Empresa(models.Model):
-    nome = models.CharField(max_length=100, help_text="Nome da empresa")
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100, help_text="Nome do Funcionario")
+
 
     def __str__(self) -> str:
         return self.nome
