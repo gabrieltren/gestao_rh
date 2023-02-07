6e45a5b5e08da9d892cb69f8424d15ef38521d83
Gabriel Trentini <gabriel.silveira1207@gmail.com>
2023-02-07 10:51:10 -0300

    Criando as apps de Documentos e Registro de hora extra

---
+++ apps/registro_hora_extra/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+
+class RegistroHoraExtra(models.Model):
+    motivo = models.CharField(max_length=100, help_text="Motivo da hora extra")
+
+    def __str__(self) -> str:
+        return self.motivo
